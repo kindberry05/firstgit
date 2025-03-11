b84c216719880480f63ba986a893e632fbad178d
eslim <kindberry@yiwoosolution.co.kr>
2025-03-11 14:40:07 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { signInWithGoogle } from "./firebaseConfig";
 function App() {
   return (
     <>
-      <h2>login</h2>
+      <h2>login2</h2>
       <button onClick={signInWithGoogle}>로그인</button>
     </>
   );
